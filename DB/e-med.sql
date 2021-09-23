f84b8f56460b711431aa289faca2e6df13f10b13
afroza36 <sathy3698@gmail.com>
2021-09-23 22:45:59 +0600

    added by afroza

--- DB/e-med.sql
+++ DB/e-med.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 23, 2021 at 05:45 PM
+-- Generation Time: Sep 23, 2021 at 06:45 PM
 -- Server version: 10.4.6-MariaDB
 -- PHP Version: 7.3.9
 
@@ -65,10 +65,7 @@ CREATE TABLE `appointment` (
 --
 
 INSERT INTO `appointment` (`id`, `user_id`, `doctor_id`, `date_time`, `serial`) VALUES
-(0, 1, 0, '2021-09-04 00:00:00', NULL),
-(1, 5, 0, '2021-09-23 03:00:00', '3'),
-(2, 1, 0, '2021-09-23 15:57:07', '1'),
-(3, 4, 0, '2021-09-23 16:13:54', '2');
+(1, 4, 0, '2021-09-23 22:08:46', '1');
 
 -- --------------------------------------------------------
 
@@ -108,15 +105,16 @@ CREATE TABLE `doctors` (
   `chamber_time_start` varchar(10) DEFAULT NULL,
   `chamber_time_end` varchar(10) DEFAULT NULL,
   `status` int(11) NOT NULL DEFAULT 0,
-  `reg_date` datetime NOT NULL DEFAULT current_timestamp()
+  `reg_date` datetime NOT NULL DEFAULT current_timestamp(),
+  `meet_link` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `doctors`
 --
 
-INSERT INTO `doctors` (`id`, `name`, `email`, `pass`, `contact_no`, `imgpath`, `token`, `featured`, `featured_date`, `speciality`, `visit_fee`, `chamber_time_start`, `chamber_time_end`, `status`, `reg_date`) VALUES
-(0, 'Afroza Sultana', 'demo@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', '0123456789', 'UIU-Logo-250.png', 'bbab683c29a53632323d2dc9339e45', 'yes', '2021-09-23', 'Neurosurgeon', '5000', '10 AM', '2 PM', 1, '2021-09-23 17:28:31');
+INSERT INTO `doctors` (`id`, `name`, `email`, `pass`, `contact_no`, `imgpath`, `token`, `featured`, `featured_date`, `speciality`, `visit_fee`, `chamber_time_start`, `chamber_time_end`, `status`, `reg_date`, `meet_link`) VALUES
+(0, 'Afroza Sultana', 'demo@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', '0123456789', 'UIU-Logo-250.png', 'bbab683c29a53632323d2dc9339e45', 'yes', '2021-09-23', 'Neurosurgeon', '5000', '10 AM', '2 PM', 1, '2021-09-23 17:28:31', NULL);
 
 -- --------------------------------------------------------
 
@@ -481,6 +479,12 @@ ALTER TABLE `wishlist`
 --
 ALTER TABLE `admin`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `appointment`
+--
+ALTER TABLE `appointment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `company`
